6c5a14f73a8aed18e68108451dfc64497241e8f5
koalabeary <171252645+koalabeary@users.noreply.github.com>
2026-04-25 21:42:22 +0200

    Klasse Aufbau hinzugefügt

--- Stundenplan/MainWindow.xaml.cs
+++ Stundenplan/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.CodeDom;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,18 +23,51 @@ namespace Stundenplan
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Erstellen_Click(object sender, RoutedEventArgs e)
+        {
+            String start = Start.Text;
+            String ende = Ende.Text;
+            Times n = new Times();
+            TimeSpan st = n.umwandlung(start);
+            TimeSpan en = n.umwandlung(ende);
+            MessageBox.Show("Die Startzeit ist: " + st + "\nDie Endzeit ist: " + en);
+
+
         }
     }
 
-    public class Times {
+    public class Times
+    {
+       public TimeSpan umwandlung(string s) {
+            try
+            {
+                char[] trenner = { ':', '.', '/', ';', ' ', '-', ',' };
+                string[] teile = s.Split(trenner);
+                if (teile.Length == 2)
+                {
 
-        int startHour;
-        int startMinute;
-        int endHour;
-        int endMinute;
-        
-        public TimeSpan Start => new TimeSpan(startHour, startMinute, 0);
-        public TimeSpan End => new TimeSpan(endHour, endMinute, 0);
+                    if (int.TryParse(teile[0], out int stunde) && int.TryParse(teile[1], out int minute))
+                        if (stunde >= 0 && stunde < 24 && minute >= 0 && minute < 60)
+                        {
+                            TimeSpan start = new TimeSpan(stunde, minute, 0);
+                            return start;
+                        }
+                }
 
+                throw new FormatException("Das Format ist ungültig. Bitte HH:mm nutzen.");
+            }
+
+            catch(Exception ex)
+            {
+                throw new Exception("Ungültige Eingabe: " + ex.Message);
+            }
+        }
     }
+
+
+
+    
 }
